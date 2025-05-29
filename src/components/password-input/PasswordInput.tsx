ef3117b4596742de3c93b3173f1c8fcf71dcb93c
rmere <rmere@griddynamics.com>
2025-05-29 13:51:00 -0600

    Add btn functionality to show/hide password

--- src/components/password-input/PasswordInput.tsx
+++ src/components/password-input/PasswordInput.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+import EyeHidden from '../../assets/svg/eye-hidden.svg?react';
+import EyeShown from '../../assets/svg/eye-shown.svg?react';
+import Button from '@/components/ui/button';
 import Input from '@/components/ui/input';
 
 export default function PasswordInput() {
+  const [isShown, setIsShown] = useState(false);
+
   return (
-    <div>
+    <div className="relative">
       <label htmlFor="password" className="hidden">
         Password
       </label>
@@ -11,7 +17,20 @@ export default function PasswordInput() {
         placeholder="Password"
         variant="login"
         className="w-full rounded-b-md"
+        type={isShown ? 'text' : 'password'}
       />
+      <Button
+        type="button"
+        variant="icon"
+        className="absolute top-1/2 right-12 -translate-y-1/2 cursor-pointer"
+        onClick={() => setIsShown((prevIsShown) => !prevIsShown)}
+      >
+        {isShown ? (
+          <EyeShown className="fill-gray hover:fill-black" />
+        ) : (
+          <EyeHidden className="fill-gray hover:fill-black" />
+        )}
+      </Button>
     </div>
   );
 }
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded font-bold transition-all cursor-pointer text-lg p-12',
+  {
+    variants: {
+      variant: {
+        default: 'bg-highlights shadow-xs hover:bg-dark-highlights text-black',
+        icon: 'bg-transparent p-0',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export default function Button({
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, className }))}
+      {...props}
+    />
+  );
+}
