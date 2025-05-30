d775c22aaf9d3a291977fdaaab087911d1c6b35f
rmere <rmere@griddynamics.com>
2025-05-29 20:19:43 -0600

    Add login footer

---
+++ src/pages/login/LoginFooter.tsx
@@ -0,0 +1,19 @@
+import GridLogo from '../../assets/svg/grid-icon.svg?react';
+import LoginForm from './login-form/LoginForm';
+
+export default function LoginFooter() {
+  return (
+    <footer className="shadow-background bg-light-gray relative z-10 flex min-h-1/2 w-full flex-col items-center justify-between">
+      <LoginForm />
+      <section className="xs:px-16 max-xs:p-16 max-xs:flex-col max-xs:items-center max-xs:gap-8 flex w-full justify-between px-36 py-32">
+        <div className="flex gap-12">
+          <GridLogo />
+          <p className="text-lg">Grid Dynamics</p>
+        </div>
+        <p className="text-gray text-lg">
+          Grid Dynamics© 2006-{new Date().getFullYear()}
+        </p>
+      </section>
+    </footer>
+  );
+}
