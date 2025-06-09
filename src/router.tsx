0152e9ec49996f028312f22e976fdc8f9b1f49f1
rmere <rmere@griddynamics.com>
2025-06-09 13:39:30 -0600

    Navigate to login when index is visited

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Login from './pages/login/Login';
 
 const router = createBrowserRouter([
+  { path: '/', element: <Navigate to="/login" replace /> },
   {
     path: '/login',
     element: <Login />,
